094b76a1a454d59cebf11767b3add7541b09c835
Rahulzhp <akumari42872@gmail.com>
2023-06-27 11:39:14 +0530

    added deployed link

--- frontend/src/Component/Mainpages.jsx
+++ frontend/src/Component/Mainpages.jsx
@@ -34,10 +34,9 @@ const Mainpages = () => {
   }
 
   useEffect(() => {
-    axios.get("http://localhost:8080/")
+    axios.get("https://splendid-shoulder-pads-mite.cyclic.app/deepthoughtassignemnt")
       .then((res) => {
-        //console.log(res.data.data[0].tasks[0].assets[0].asset_content)
-        setData(res.data.data[0])
+        setData(res)
       })
       .catch((err) => {
         console.log(err)
@@ -76,9 +75,9 @@ const Mainpages = () => {
                 <div>
                   <div>
                     <div>
-                      <li className='firstTitle'>{data.tasks[0] && data.tasks[0].task_title}</li>
+                      <li className='firstTitle'>{data && data.data && data.data.tasks && data.data.tasks[0].task_title}</li>
                       {
-                        data.tasks[0] && data.tasks[0].assets.map((el) => {
+                        data && data.data && data.data.tasks && data.data.tasks[0].assets.map((el) => {
                           return (
                             <div key={el.task_id} className="titlelist">
                               <li>{el.asset_title}</li>
@@ -95,14 +94,13 @@ const Mainpages = () => {
 
 
         <div onClick={handleClicktwo}>
-          <Heading headingtitle={data.tasks && data.tasks[0].title} secondheading={data.tasks && data.tasks[0].task_title} paragraph={data.tasks && data.tasks[0].task_description} />
+          <Heading headingtitle={data && data.data && data.data.title} secondheading={data && data.data && data.data.tasks && data.data.tasks[0].task_title} paragraph={data && data.data && data.data.tasks && data.data.tasks[0].task_description} />
           <div className='mainBody'>
-
             {
-              data && data.tasks && data.tasks[0].assets.map((el, index) => {
+              data && data.data && data.data.tasks && data.data.tasks[0].assets.map((el, index) => {
                 return (
-                  <div key={el.id}>
-                    <Assets heading={el.asset_title} description={el.asset_description} children={index === 0 ? <Youtube link={el.asset_content} /> : index === 1 ? <Thredbuilder /> : index === 2 ? <Structure /> : index === 3 ? <FourSA /> : ""} />
+                  <div key={el.task_id}>
+                    <Assets heading={el.asset_title} description={el.asset_description} children={index === 0 ? <Youtube link={el.asset_content} /> : index === 1 ? <Thredbuilder /> : index === 2 ? <Structure /> : index === 3 ? <FourSA /> : null} />
                   </div>
                 )
               })
